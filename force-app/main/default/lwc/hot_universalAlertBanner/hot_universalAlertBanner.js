79f84eb4ed21e17fbfd67b1d418d804d73010348
Sebastian Hoxhallari <sebastian.hoxhallari@nav.no>
2026-09-01 09:54:29 +0200

    Add hot_universalAlertBanner component
    Add textSize property

---
+++ force-app/main/default/lwc/hot_universalAlertBanner/hot_universalAlertBanner.js
@@ -0,0 +1,91 @@
+import { LightningElement, api } from 'lwc';
+import icons from '@salesforce/resourceUrl/aksel_ikoner';
+
+export default class Hot_universalAlertBanner extends LightningElement {
+    @api type = 'Info';
+    @api message = '';
+    @api assistiveText;
+    @api closeButton = false;
+    @api showIcon = false;
+    @api backgroundColor;
+    @api textSize = 'small';
+
+    announcementIcon = icons + '/Media/MegaphoneSpeakingFillWhite.svg';
+    successIcon = icons + '/Status/CheckmarkCircleFillWhite.svg';
+    warningIcon = icons + '/Status/ExclamationmarkTriangleFill.svg';
+    errorIcon = icons + '/Status/XMarkOctagonFillWhite.svg';
+
+    isClosed = false;
+
+    get normalizedType() {
+        const normalized = (this.type || 'info').toLowerCase();
+        if (normalized === 'warning' || normalized === 'error' || normalized === 'info') {
+            return normalized;
+        }
+        return 'info';
+    }
+
+    get isVisible() {
+        return !this.isClosed;
+    }
+
+    get shouldShowIcon() {
+        return this.showIcon !== false && this.showIcon !== 'false';
+    }
+
+    get textSizeClass() {
+        return `banner__content--${this.textSize}`;
+    }
+
+
+    get resolvedIconName() {
+        if (this.iconName) {
+            return this.iconName;
+        }
+
+        switch (this.normalizedType) {
+            case 'warning':
+                return 'utility:warning';
+            case 'error':
+                return 'utility:error';
+            default:
+                return 'utility:info';
+        }
+    }
+
+    get showCloseButton() {
+        return this.closeButton !== false && this.closeButton !== 'false';
+    }
+
+    get bannerClass() {
+        return `banner banner--${this.normalizedType}`;
+    }
+
+    get resolvedAssistiveText() {
+        return this.assistiveText || this.type || 'Info';
+    }
+
+    get bannerStyle() {
+        if (!this.backgroundColor) {
+            return undefined;
+        }
+
+        return `--banner-background: ${this.backgroundColor};`;
+    }
+
+    get iconUrl() {
+        switch (this.normalizedType) {
+            case 'warning':
+                return this.warningIcon;
+            case 'error':
+                return this.errorIcon;
+            default:
+                return this.announcementIcon;
+        }
+    }
+
+    handleClose() {
+        this.isClosed = true;
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+}
